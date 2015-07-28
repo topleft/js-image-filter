3273b469863012731eb6f051ad58f5c680d51d03
Luis <delapenl@gmail.com>
2015-07-28 16:56:04 -0600

    first part of the problem

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+function filter(array,amountHigh) {
+	var houses = [];
+	console.log(amountHigh);
+	var amountLow = 0;
+
+	if (amountHigh === 50000){
+		amountLow = 0;
+	}
+	else if (amountHigh === 100000) {
+		amountLow = 50000;
+	}
+	else if (amountHigh === 200000) {
+		amountLow = 100000;
+	}
+	else if (amountHigh === 300000) {
+		amountLow = 200000;
+	}
+	else if (amountHigh === 500000) {
+		amountLow = 300000;
+	}
+	else if (amountHigh === 1000000) {
+		amountLow = 500000;
+	}
+
+	for (var i = 0; i < array.length; i++) {
+		var house = parseInt($(array[i]).attr("data-price"));
+		if(house >= amountLow && house <= amountHigh){
+			// console.log(typeof house);
+			// console.log(house)
+			houses.push(house);
+			// debugger
+		}
+	}
+	console.log(houses)
+	return houses;
+}
+
+
+
 $(document).ready(function() {
-	console.log("The document is ready!");
-})
+	var homeArray =	$('.image-container').find("article");
+		// console.log(filter(homeArray, 50000, 100000));
+
+
+	var checkBoxes = $(".filters").find("input");
+	// console.log(checkBoxes);
+
+	$(checkBoxes).on("click", function(){
+		var high = parseInt($(this).val());
+		filter(homeArray,($(high)[0]));
+	});
+});
