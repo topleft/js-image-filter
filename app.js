a5cd80b100c9b423cca431ea47060d0d1cd77910
topleft <pete.james.jeffryes@gmail.com>
2015-08-09 22:50:52 -0600

    fix bug, page was loading app.js twice

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 
 $(document).ready(function(){
 
+	// add background texture option //
+	$(".textures").append("<span class='forest'></span>");
+
 	var houses = $("article");
 
-	console.log(houses);
 	//create new houses
 	$(".image-container").append(
 		$(createHouse(houses, "320000", "img/four.jpg", "$320,000")),
@@ -12,10 +14,7 @@ $(document).ready(function(){
 				$(createHouse(houses, "225000", "img/four.jpg", "$150,000"))
 	);
 
-	// houses = $("article");
 
-	// add background texture option //
-	$(".textures").append("<span class='forest'></span>");
 
 	// change background texture //
 	$('.textures span').on('click',function(e) {
@@ -37,8 +36,8 @@ $(document).ready(function(){
 		  default:
 		    break;
 		}
-});
-});
+	});
+
 
 // filter by price
 	$(".filters input").on('click', function(){
@@ -47,6 +46,7 @@ $(document).ready(function(){
 		houses.hide();
 
 		filterByPrice(checked, houses);
+	});
 
 
 	//sort houses by price
@@ -63,56 +63,11 @@ $(document).ready(function(){
 	});
 
 // end of document ready
+
+
 });
 
 
-
-
-function filterByPrice(checked, houses){
-		// make sure something is selected
-	if(checked.length > 0){
-		//loop through checked boxes
-		for (var j = 0; j < checked.length; j++) {
-			// set high price for current checked box
-			var hiPrice = parseInt($(checked[j]).val());
-			// look for 0-50,000 filter, it doesn't have upper sibling, needs to be set manually
-			if(hiPrice === 50000)
-				var lowPrice = 0;
-			else
-				var lowPrice = parseInt($(checked[j]).parent().prev().find("input")[0].value);
-
-			for (var i = 0; i < houses.length; i++) {
-				var housePrice = parseInt($(houses[i])[0].dataset.price);
-				if(housePrice <= hiPrice && housePrice >= lowPrice){
-					$(houses[i]).show();
-				};
-			}
-		};
-	}
-	else $("article").show();
-};
-
-function createHouse(array, data, jpg, priceTag){
-	var newHome = ($(array[0]).clone())[0];
-	$(newHome).attr("data-price", data);
-	$(newHome).find("img").attr("src", jpg);
-	$(newHome).find("h4").html(priceTag);
-	return newHome;
-};
-
-function highLow (array, sort) {
-	if (sort === "low"){
-		var sorted = array.sort(function (a, b) {
-			return $(a).data("price") - $(b).data("price");
-		});
-	}
-	else {
-		var sorted = array.sort(function (a, b) {
-			return $(b).data("price") - $(a).data("price");
-		});
-	}
-	return sorted;
-}
 
 
 
